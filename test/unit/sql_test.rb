603c96c87eb20418f3836d179f367d8f409157fb
Christopher Kintner <ckintner@zendesk.com>
2014-09-22 15:06:18 -0700

    use minitest

--- test/unit/sql_test.rb
+++ test/unit/sql_test.rb
@@ -1,32 +1,39 @@
-require "test_helper"
+require_relative "../helper"
 
 class SqlTest < ActiveSupport::TestCase
-  fixtures :users
+  class RestrictedUser < User
+    #arsi(:require => :account_id, /account_id/)
+  end
 
-  context "SQL updates" do
-    should "fail without an account_id" do
-      assert_raise RuntimeError do
-        assert User.delete_all(:title => 'hello')
-      end
+  def setup
+  end
 
-      assert_raise RuntimeError do
-        assert User.update_all(:title => 'hello')
-      end
+  def teardown
+    User
+  end
+
+  def test_sql_updates_should_fail_without_an_account_id
+    assert_raise RuntimeError do
+      assert RestrictedUser.delete_all(:title => 'hello')
     end
 
-    should "not fail with an account_id" do
-      assert User.update_all({ :title => 'hello' }, :account_id => 1)
-      assert User.delete_all(:account_id => 1)
+    assert_raise RuntimeError do
+      assert RestrictedUser.update_all(:title => 'hello')
     end
+  end
 
-    should "not fail with an id" do
-      assert entries(:ponies).update_attributes(:title => 'hello')
-      assert entries(:ponies).destroy
-    end
+  def test_sql_updates_should_not_fail_with_an_account_id
+    assert RestrictedUser.update_all({ :title => 'hello' }, :account_id => 1)
+    assert RestrictedUser.delete_all(:account_id => 1)
+  end
 
-    should "not fail with without_id_check" do
-      assert User.without_id_check.update_all(:title => 'hello')
-      assert User.without_id_check.delete_all(:title => 'hello')
-    end
+  def test_sql_update_should_not_fail_with_an_account_id
+    assert entries(:ponies).update_attributes(:title => 'hello')
+    assert entries(:ponies).destroy
+  end
+
+  def test_should_not_fail_with_without_id_check
+    assert RestrictedUser.without_id_check.update_all(:title => 'hello')
+    assert RestrictedUser.without_id_check.delete_all(:title => 'hello')
   end
 end
