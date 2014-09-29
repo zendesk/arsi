445850db26a0a21b7874711095d62fdac1355f0a
Christopher Kintner <ckintner@zendesk.com>
2014-09-29 14:40:05 -0700

    fix loading error

--- lib/arsi/arel_tree_manager.rb
+++ lib/arsi/arel_tree_manager.rb
@@ -1,8 +1,10 @@
-module Arsi::ArelTreeManager
-  # copied from Arel::SelectManager
-  def where_sql
-    return if @ctx.wheres.empty?
-    viz = Arel::Visitors::WhereSql.new @engine.connection
-    Arel::Nodes::SqlLiteral.new viz.accept @ctx
+module Arsi
+  module ArelTreeManager
+    # copied from Arel::SelectManager
+    def where_sql
+      return if @ctx.wheres.empty?
+      viz = Arel::Visitors::WhereSql.new @engine.connection
+      Arel::Nodes::SqlLiteral.new viz.accept @ctx
+    end
   end
 end
