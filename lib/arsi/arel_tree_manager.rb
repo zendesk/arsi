23396645e6afadaf3e14aeb5b2d87d41e9760707
Jim Gay <jim@saturnflyer.com>
2020-02-14 16:03:13 -0500

    Update handling of arel engine
    Arel 9 does not have an engine accessor.
    Arel 6 uses only the connection for new WhereSql objects.
    The provided argument to where_sql should be used over the default

--- lib/arsi/arel_tree_manager.rb
+++ lib/arsi/arel_tree_manager.rb
@@ -5,13 +5,23 @@ module Arsi
     # This is from Arel::SelectManager which inherits from Arel::TreeManager.
     # We need where_sql on both Arel::UpdateManager and Arel::DeleteManager so we add it to the parent class.
     if ::Arel::VERSION[0].to_i >= 6
-      def where_sql(engine = Arel::Table.engine)
+      def where_sql(provided_engine = :none)
         return if @ctx.wheres.empty?
 
-        viz = if ::Arel::VERSION[0].to_i > 6
-          ::Arel::Visitors::WhereSql.new(engine.connection.visitor, engine.connection)
+        selected_engine = provided_engine
+        if selected_engine == :none
+          # Arel 9 has no engine reader
+          selected_engine = if self.respond_to?(:engine)
+            self.engine || Arel::Table.engine
+          else
+            Arel::Table.engine
+          end
+        end
+
+        viz = if ::Arel::VERSION.first.to_i == 6
+          ::Arel::Visitors::WhereSql.new selected_engine.connection
         else
-          ::Arel::Visitors::WhereSql.new @engine.connection
+          ::Arel::Visitors::WhereSql.new(selected_engine.connection.visitor, selected_engine.connection)
         end
         ::Arel::Nodes::SqlLiteral.new viz.accept(@ctx, ::Arel::Collectors::SQLString.new).value
       end
