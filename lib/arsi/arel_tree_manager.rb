e3b3178ad9f4ae82154b4461f18631420ce90c69
Jim Gay <jim@saturnflyer.com>
2020-02-19 11:27:16 -0500

    Switch to checking Arel versions

--- lib/arsi/arel_tree_manager.rb
+++ lib/arsi/arel_tree_manager.rb
@@ -2,6 +2,10 @@ require 'arel'
 
 module Arsi
   module ArelTreeManager
+    AREL_WHERE_SQL_ARITY_1 = ::Arel::VERSION[0].to_i < 7
+    # Arel 9 has no engine reader
+    AREL_WHERE_SQL_ENGINE_ACCESSOR = ::Arel::VERSION[0].to_i < 9
+
     # This is from Arel::SelectManager which inherits from Arel::TreeManager.
     # We need where_sql on both Arel::UpdateManager and Arel::DeleteManager so we add it to the parent class.
     def where_sql(provided_engine = :none)
@@ -9,15 +13,14 @@ module Arsi
 
       selected_engine = provided_engine
       if selected_engine == :none
-        # Arel 9 has no engine reader
-        selected_engine = if self.respond_to?(:engine)
+        selected_engine = if AREL_WHERE_SQL_ENGINE_ACCESSOR
           self.engine || Arel::Table.engine
         else
           Arel::Table.engine
         end
       end
 
-      viz = if ::Arel::Visitors::WhereSql.instance_method(:initialize).arity == 1
+      viz = if AREL_WHERE_SQL_ARITY_1
         ::Arel::Visitors::WhereSql.new selected_engine.connection
       else
         ::Arel::Visitors::WhereSql.new(selected_engine.connection.visitor, selected_engine.connection)
